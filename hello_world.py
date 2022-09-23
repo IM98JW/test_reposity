c9fee56be6f407cc3db9e5e5aeeff6593ed182c8
IM98JW <113347838+IM98JW@users.noreply.github.com>
2022-09-23 09:38:58 +0900

    파일 1

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+def print_hello():
+    print("Hello World!!")
+
+def print_kit():
+    print("Kumoh National Institute of Technology")
+
+if __name__ == '__main__':
+    print_kit()
+    print_hello()
