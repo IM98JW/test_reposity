5c9bbcc306c808040beedd342483536eee0adf7a
IM98JW <113347838+IM98JW@users.noreply.github.com>
2022-09-23 09:26:03 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,5 @@
-def print_hello():
-    print("Hello World!!")
-
 def print_KIT():
     print("Kumoh national Institute of Technology")
 
 if __name__ == '__main__':
     print_KIT()
-    print_hello()
